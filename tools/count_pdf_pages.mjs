ee0337393dfd7a8e84e5357614599b8c0038afeb
Polina <polinarozec.co.jp>
2026-05-12 16:22:46 +0900

    cleanup

--- tools/count_pdf_pages.mjs
+++
@@ -1,7 +0,0 @@
-import { readFile } from 'node:fs/promises';
-
-for (const file of process.argv.slice(2)) {
-    const content = await readFile(file, 'latin1');
-    const pages = content.match(/\/Type\s*\/Page\b/g)?.length ?? 0;
-    console.log(`${file}: ${pages} pages`);
-}
